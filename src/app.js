2f2c2e259ea9b87fa8cea9e0cfb29862b44fce23
Alokkagarwal18 <Alokagarwal1812@gmail.com>
2025-10-22 15:21:30 +0530

    Authentication using JWT

--- src/app.js
+++ src/app.js
@@ -3,30 +3,32 @@ const connectDB = require("./config/database.js");
 const app = express();
 const User = require("./models/user.js");
 const { validateSignUpData } = require("./utils/validation.js");
+const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.post("/signup", async (req, res) => {
-
   try {
-  // validate the signup data
-  validateSignUpData(req);
+    // validate the signup data
+    validateSignUpData(req);
 
-  const { firstName, lastName, emailId, password} = req.body;
-  // Encrypt the password
+    const { firstName, lastName, emailId, password } = req.body;
+    // Encrypt the password
 
-  const passwordHash = await bcrypt.hash(password, 10);
+    const passwordHash = await bcrypt.hash(password, 10);
 
-  //crating a new instance of User model
-  const user = new User({
-    firstName,
-    lastName,
-    emailId,
-    password: passwordHash,
+    //crating a new instance of User model
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
 
-  });
-
-  // it always give us t oa promise so handle the promise we used async await
+    // it always give us t oa promise so handle the promise we used async await
     await user.save();
     res.send("User signed up successfully");
   } catch (err) {
@@ -34,28 +36,56 @@ app.post("/signup", async (req, res) => {
   }
 });
 
-
 app.post("/login", async (req, res) => {
-    try {
-        const { emailId, password } = req.body;
+  try {
+    const { emailId, password } = req.body;
 
-        validateSignUpData(req);
+    validateSignUpData(req);
 
-        const user = await User.findOne({ emailId: emailId });
-        if(!user){
-          throw new Error("Invalid Credentials")
-        }
-        const isPasswordValid = bcrypt.compare(password, user.password);
-        if(isPasswordValid){
-            res.send("Login Successful");
-        }
-        else{
-          throw new Error("Invalid Credentials");
-        }
-    } catch (err) {
-        res.status(400).send("ERROR:" + err.message);
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("Invalid Credentials");
     }
-})
+    const isPasswordValid = bcrypt.compare(password, user.password);
+    if (isPasswordValid) {
+      // create JWT token
+      const token = await jwt.sign({ id: user._id }, "JWT_SECRET_KEY123");
+      console.log(token);
+
+      // Add the token to cookie and send the response back to the user
+      res.cookie("token", token);
+      res.send("Login Successful");
+    } else {
+      throw new Error("Invalid Credentials");
+    }
+  } catch (err) {
+    res.status(400).send("ERROR:" + err.message);
+  }
+});
+
+app.get("/profile", async (req, res) => {
+  try {
+    const cookies = req.cookies;
+    const { token } = cookies;
+
+    if (!token) {
+      throw new Error("Invalid Token");
+    }
+
+    const decodedMessage = await jwt.verify(token, "JWT_SECRET_KEY123");
+
+    const { _id } = decodedMessage;
+
+    const user = User.findById(_id);
+    if (!user) {
+      throw new Error("User doest not exist");
+    }
+
+    res.send(user);
+  } catch (error) {
+    res.status(400).send("ERROR:" + err.message);
+  }
+});
 
 // get user by email
 app.get("/user", async (req, res) => {
@@ -102,28 +132,19 @@ app.patch("/user/:userId", async (req, res) => {
   const userId = req.params?.userId;
   const data = req.body;
 
-  const ALLOWED_UPDATES = [
-    "photpUrl",
-    "about",
-    "gender",
-    "age",
-    "skills",
-  ];
-
-
+  const ALLOWED_UPDATES = ["photpUrl", "about", "gender", "age", "skills"];
 
   try {
-    
-  // looping through each key and i will make sure that every key present in the ALLOWED_UPDATES
+    // looping through each key and i will make sure that every key present in the ALLOWED_UPDATES
     const isUpdateAlllowed = Object.keys(data).every((k) =>
       ALLOWED_UPDATES.includes(k)
     );
 
     if (!isUpdateAlllowed) {
-      throw new Error("Update not Allowed")
+      throw new Error("Update not Allowed");
     }
 
-    if(data.skills.length > 10){
+    if (data.skills.length > 10) {
       throw new Error("Skills can't be more than 10");
     }
 
